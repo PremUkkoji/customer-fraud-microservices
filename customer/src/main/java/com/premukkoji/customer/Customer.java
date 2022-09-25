b9052362a40ea302ba6505c28658c52519d6b7e0
Prem Ukkoji <premukkoji@gmail.com>
2022-09-25 11:26:45 +0530

    customer module created

---
+++ customer/src/main/java/com/premukkoji/customer/Customer.java
@@ -0,0 +1,15 @@
+package com.premukkoji.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
